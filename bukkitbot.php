581db95bed4889db9def7a96e8feac5f0b76de07
Tim Murtaugh <tim@o2b.net>
2014-08-11 11:38:05 -0400

    update bukkitbot

--- bukkitbot.php
+++ bukkitbot.php
@@ -2,71 +2,96 @@
 
 $foundone = 'null';
 
-$file = 'http://bukk.it/' . $_GET["text"];
-$file_headers = @get_headers($file);
-if($file_headers[0] != 'HTTP/1.1 404 Not Found') {
-   
-	$foundone = $file;
+$bukkitBaseURL = 'http://bukk.it/';
 
-};
+$channelToken = 'zgZtHd0u3l5KIbaHmpbIfIZH';
 
-$file = 'http://bukk.it/' . $_GET["text"] . '.jpg';
-$file_headers = @get_headers($file);
-if($file_headers[0] != 'HTTP/1.1 404 Not Found') {
-   
-	$foundone = $file;
+$channel = $_GET["channel_name"];
 
-};
+$username = $_GET["user_name"];
 
-$file = 'http://bukk.it/' . $_GET["text"] . '.jpeg';
-$file_headers = @get_headers($file);
-if($file_headers[0] != 'HTTP/1.1 404 Not Found') {
-   
-	$foundone = $file;
+$request = $_GET["text"];
 
-};
+$header404 = 'HTTP/1.1 404 Not Found';
 
-$file = 'http://bukk.it/' . $_GET["text"] . '.png';
-$file_headers = @get_headers($file);
-if($file_headers[0] != 'HTTP/1.1 404 Not Found') {
-   
-	$foundone = $file;
+if ($request == "-help") {
 
-};
+	echo "_BukkitBot -help:_ use either a whole file name (e.g. `clap.gif`) or you can leave the extension off (e.g. `clurb`) and BukkitBot will try to find a match. In the case of multiple matches (e.g. `catbus`) it will prioritize a gif over other file types. Use `-popular` to see some examples.";
 
-$file = 'http://bukk.it/' . $_GET["text"] . '.gif';
-$file_headers = @get_headers($file);
-if($file_headers[0] != 'HTTP/1.1 404 Not Found') {
-   
-	$foundone = $file;
+} else if ($request == "-popular") {
 
-};
+	echo "_BukkitBot -popular:_ `allthefucks`, `ameliewait`, `bale-kermit`, `beaker-freak`, `caruso`, `clap`, `facepalm`, `golfclap`, `gotime`, `gunn-cant`, `i-cant`, `imout`, `irl.jpg`, `jakedance`, `joy`, `lineup`, `maaaaaaaagic`, `mandybrown`, `mindblown`, `needhugs`, `no`, `nopenope`, `notallfish`, `omg`, `omgjake`, `oooooooo`, `party`, `pizzaaa`, `sad`, `shutup`, `skeletor`, `slayer`, `success`, `thinkin`, `umad`, `umguys`, `wat2`, `welp`, `whar.jpg`, `what.jpg`, `why`, `wilto`, `wonkagtfo`, `yay`, `yayyy`, `yes`, `zen`, `zeroregrets`";
 
-if ($foundone == "null") {
+} else if ($channel != 'general') {
 
-	echo "BukkitBot looked for " . $_GET["text"] . " but couldn't find an image :(";
+	echo "Sorry, BukkitBot only works in the #general channel for the moment.";
 
 } else {
 
-	$channel = $_GET["channel_name"];
-	$username = $_GET["user_name"];
-
-	$ch = curl_init();
-	$curlConfig = array(
-	    CURLOPT_URL            => "https://alistapart.slack.com/services/hooks/incoming-webhook?token=zgZtHd0u3l5KIbaHmpbIfIZH",
-	    CURLOPT_POST           => true,
-	    CURLOPT_RETURNTRANSFER => true,
-	    CURLOPT_POSTFIELDS     => json_encode(array(
-	       'text' => $foundone,
-	       'username' => 'bukkit',
-		   'channel_name' => $channel,
-		   'username' => $username,
-	    ))
-	);
-
-	curl_setopt_array($ch, $curlConfig);
-	$result = curl_exec($ch);
-	curl_close($ch);
+	$file = $bukkitBaseURL . $request;
+	$file_headers = @get_headers($file);
+	if($file_headers[0] != $header404) {
+	   
+		$foundone = $file;
+	
+	};
+	
+	$file = $bukkitBaseURL . $request . '.jpg';
+	$file_headers = @get_headers($file);
+	if($file_headers[0] != $header404) {
+	   
+		$foundone = $file;
+	
+	};
+	
+	$file = $bukkitBaseURL . $request . '.jpeg';
+	$file_headers = @get_headers($file);
+	if($file_headers[0] != $header404) {
+	   
+		$foundone = $file;
+	
+	};
+	
+	$file = $bukkitBaseURL . $request . '.png';
+	$file_headers = @get_headers($file);
+	if($file_headers[0] != $header404) {
+	   
+		$foundone = $file;
+	
+	};
+	
+	$file = $bukkitBaseURL . $request . '.gif';
+	$file_headers = @get_headers($file);
+	if($file_headers[0] != $header404) {
+	   
+		$foundone = $file;
+	
+	};
+	
+	if ($foundone == "null") {
+	
+		echo "BukkitBot looked for `" . $request . "` on `" . $bukkitBaseURL . "` but couldn't find an image :sob:";
+	
+	} else {
+	
+		$ch = curl_init();
+		$curlConfig = array(
+		    CURLOPT_URL            => "https://alistapart.slack.com/services/hooks/incoming-webhook?token=" . $channelToken,
+		    CURLOPT_POST           => true,
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_POSTFIELDS     => json_encode(array(
+		       'text' => $foundone,
+		       'username' => 'bukkit',
+			   'channel_name' => $channel,
+			   'username' => $username,
+		    ))
+		);
+	
+		curl_setopt_array($ch, $curlConfig);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	
+	}
 
 }
 
